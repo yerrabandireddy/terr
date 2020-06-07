b58366d0696de247116948e9f31feb2e6266d362
kaza514 <63160591+kaza514@users.noreply.github.com>
2020-06-07 18:29:02 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  description = "To use AWS region"
+  default = "ap-south-1"
+}
+
+variable "no_of_instances" {
+default = "3"
+}
+
+variable "ami" {
+default = "ami-0447a12f28fddb066"
+}
+
+variable "instance_type" {
+default = "t2.micro"
+}
+
+
+variable "key" {
+description = "Server Key file name"
+default = "Sec_key"
+}
+
+variable "bootstrap_path" {
+  description = "Script to install startup"
+  default = "demo.sh"
+}
