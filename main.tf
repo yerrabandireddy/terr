fbc44f6a71fc70b80c7c265afbc2f007b3aa8dfd
kaza514 <63160591+kaza514@users.noreply.github.com>
2020-06-07 20:04:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,15 @@ provider  "aws" {
 }
 
 resource "aws_instance" "testinstance"{
-   count = 3
+#   count = 3
    ami = "${var.ami}"
    instance_type = "${var.instance_type}"
    key_name = "${var.key}"
    user_data = "${file("${var.bootstrap_path}")}"
 
    tags = {
-        Name = "Server-${count.index}"
-        }
+#        Name = "Server-${count.index}"
+         Name = "Server"
+      }
 
 }
